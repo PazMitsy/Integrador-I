7e104643eeb15894770a08fc52789788b733bd3b
PazMitsy <pmich9872@gmail.com>
2025-05-13 18:11:15 -0500

    Add files via upload

---
+++ ConexionBD/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "";
+$dbname = "control_gastos_familiares";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
